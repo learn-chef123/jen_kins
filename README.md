284f438427154621147db2835801bdfce2769e0a
okram999 <nirish.okram@gmail.com>
2016-12-13 10:54:47 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jen_kins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: jen_kins
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
